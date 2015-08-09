fd53c0398879c81a6418640fcd4ba11c4b08d9bd
SindhoojaMullapudi <Imsaisindhu@gmail.com>
2015-08-08 20:04:20 -0700

    Added 3 more classes and 12 more properties

---
+++ Codekeeper/Codekeeper/Codekeeper.Shared/Models/PatientInformation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codekeeper.Models
+{
+    public class PatientInformation
+    {
+        /// <summary>
+        /// This property stores the first name of the patient
+        /// </summary>
+        public String FirstName { get; set; }
+
+        /// <summary>
+        /// this property stores the last name of the patient
+        /// </summary>
+        public String LastName { get; set; }
+
+        /// <summary>
+        /// this property stores the date of birth of the patient
+        /// </summary>
+        public String DOB { get; set; }
+
+        /// <summary>
+        /// this property stores the gender type of the patient
+        /// </summary>
+        public String Gender { get; set; }
+
+        /// <summary>
+        /// this property stores the Social Security Number of the patient
+        /// </summary>
+        public int SSN { get; set; }
+
+        /// <summary>
+        /// this property stores the Address of the patient
+        /// </summary>
+        public String Address { get; set; }
+
+        /// <summary>
+        /// this property stores the medical history of the patient
+        /// </summary>
+        public String History { get; set; }
+
+        /// <summary>
+        /// this property contain a list of all the medications the patients has taken-- could be long term and short term
+        /// </summary>
+        public String MedicationList { get; set; }
+
+        /// <summary>
+        /// this property will list out all the allergies the patient might have
+        /// </summary>
+        public String Allergies { get; set; }
+    }
+}
