4d85469f0e8e6ad8775dc11753420f1abca11a87
Kal <info@kalacademy.org>
2015-08-15 16:34:48 -0700

    Added validation for number

--- Codekeeper/Codekeeper/Codekeeper.Windows/MainPage.xaml.cs
+++ Codekeeper/Codekeeper/Codekeeper.Windows/MainPage.xaml.cs
@@ -30,21 +30,37 @@ namespace Codekeeper
 
         private void txtHour_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
+            //Added validaiton for number
+            //Sindhu - you add validation for 0-23 and also do similar checks for minutes
+            uint hour;
+            if (!uint.TryParse(txtHour.Text, out hour))
+            {
+                if (txtHour.Text.Length > 1)
+                {
+                    txtHour.Text = txtHour.Text.Substring(0, txtHour.Text.Length - 1);
+                }
+                else
+                {
+                    txtHour.Text = string.Empty;
+                }
+            }
 
         }
 
         private async void btnStart_Click(object sender, RoutedEventArgs e)
         {
             MessageDialog md = new MessageDialog("Confirm the CPR start time?");
-            md.Commands.Add(new UICommand("Yes"));
+            md.Commands.Add(new UICommand("Yes", YesBtnClick));
             md.Commands.Add(new UICommand("No"));
             await md.ShowAsync();
-
         }
 
  
+        private void YesBtnClick(IUICommand command)
+        {
+            //Sindhu - create a new instance of "Code" here and set its CPR start time to the time that was entered in the hour and minute box
 
+        }
 
     }
 }
