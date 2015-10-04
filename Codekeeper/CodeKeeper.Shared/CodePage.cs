367700d5e794ce08ed5bf6abd685595f5321fd61
SindhoojaMullapudi <Imsaisindhu@gmail.com>
2015-10-04 11:18:41 -0700

    Added color to the defib page

--- Codekeeper/CodeKeeper.Shared/CodePage.cs
+++ Codekeeper/CodeKeeper.Shared/CodePage.cs
@@ -23,7 +23,7 @@ namespace Codekeeper
         {
             Current = this;
             AppBar timeTracker = new AppBar();
-            timeTracker.Background = new SolidColorBrush(new Color { A = 33, R = 3, G = 0, B = 0 });
+            timeTracker.Background = new SolidColorBrush(new Color { A = 33, R = 118, G = 15, B = 5 });
             var ttControl = new TimeTrackerControl();
             ttControl.CPRStartTime = CurrentCode.CPRStartTime.ToString();
             timeTracker.Content = ttControl;
