c37ff9ec9dd56c4e7c162c7d6fde07ca81b0d3d8
SindhoojaMullapudi <Imsaisindhu@gmail.com>
2015-10-04 12:35:35 -0700

    added new class-- Resuscitation

---
+++ Codekeeper/CodeKeeper.Shared/Models/Resuscitation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codekeeper.Models
+{
+    public class Resuscitation
+    {
+        public DateTime TimeRecorded { get; set; }
+        public string ResType { get; set; }
+
+    }
+}
